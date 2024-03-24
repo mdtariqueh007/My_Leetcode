6ec7334b403947bfc407cab465cc7f558a211bd7
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-03-24 11:51:14 +0530

    Time: 33 ms (16.67%) | Memory: 19.1 MB (16.67%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+
+
+        int n = s.length();
+
+        int ans = 0;
+
+        for(int i = 0;i<n;i++){
+            unordered_map<char,int> mp;
+
+            for(int j = i;j<n;j++){
+                mp[s[j]]++;
+
+                if(mp[s[j]]>2){
+                    break;
+                }
+
+                ans = max(ans, j - i + 1);
+            }
+        }
+
+        return ans;
+        
+        
+    }
+};
