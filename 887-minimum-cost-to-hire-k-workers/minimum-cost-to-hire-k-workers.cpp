833db95a84077b32d28f394816389baa63d45662
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-05-11 23:51:37 +0530

    Time: 22 ms (84.64%) | Memory: 26.4 MB (68.16%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -5,24 +5,24 @@ public:
 
         vector<pair<double,int>> ratio;
 
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;i++){//O(n)
             ratio.push_back({wage[i]*1.0/quality[i]*1.0,quality[i]});
         }
 
-        sort(ratio.begin(),ratio.end());
+        sort(ratio.begin(),ratio.end()); //O(nlogn)
 
         priority_queue<int> pq;
 
         double sum = 0.0;
 
-        for(int i = 0;i<k-1;i++){
+        for(int i = 0;i<k-1;i++){ //O(klogk)
             pq.push(ratio[i].second);
             sum += ratio[i].second;
         }
 
         double ans = 1e9;
 
-        for(int i = k-1;i<n;i++){
+        for(int i = k-1;i<n;i++){//O(nlogk)
             pq.push(ratio[i].second);
             sum += ratio[i].second;
             if(pq.size()>k){
