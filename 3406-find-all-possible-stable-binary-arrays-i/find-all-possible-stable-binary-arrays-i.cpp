734c3ecbdc523c059fb41ee1496d4c29d774bb1b
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-05-12 16:10:49 +0530

    Time: 388 ms (42.19%) | Memory: 44 MB (61.91%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -2,7 +2,7 @@ class Solution {
     private:
     // int dp[201][201][3][201];
     const int mod = 1e9+7;
-    long long solve(int zero,int one,int prev,int cnt,int limit,vector<vector<vector<vector<int>>>>&dp){
+    long long solve(int zero,int one,int prev,int limit,vector<vector<vector<int>>>&dp){
         if(zero==0 && one==0){
             return 1;
         }
@@ -11,31 +11,25 @@ class Solution {
             return 0;
         }
 
-        if(dp[zero][one][prev][cnt]!=-1){
-            return dp[zero][one][prev][cnt];
+        if(dp[zero][one][prev]!=-1){
+            return dp[zero][one][prev];
         }
 
         long long ans = 0;
 
-        if(zero>0){
-            if(prev==0 && cnt<limit){
-                ans = (ans + solve(zero-1,one,0,cnt+1,limit,dp))%mod;
-            }
-            else if(prev==1 || cnt==0){
-                ans = (ans + solve(zero-1,one,0,1,limit,dp))%mod;
+        if(prev!=0){
+            for(int cnt = 1;cnt<=min(zero,limit);cnt++){
+                ans = (ans + solve(zero-cnt,one,0,limit,dp))%mod;
             }
         }
 
-        if(one>0){
-            if(prev==1 && cnt<limit){
-                ans = (ans + solve(zero,one-1,1,cnt+1,limit,dp))%mod;
-            }
-            else if(prev==0 || cnt==0){
-                ans = (ans + solve(zero,one-1,1,1,limit,dp))%mod;
+        if(prev!=1){
+            for(int cnt = 1;cnt<=min(one,limit);cnt++){
+                ans = (ans + solve(zero,one-cnt,1,limit,dp))%mod;
             }
         }
 
-        return dp[zero][one][prev][cnt] = ans%mod;
+        return dp[zero][one][prev]= ans%mod;
     }
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
@@ -44,8 +38,8 @@ public:
 
         // long long ans = solve(zero,one,2,0,limit);
 
-        vector<vector<vector<vector<int>>>>dp(zero+1,vector<vector<vector<int>>>(one+1, vector<vector<int>>(3,vector<int>(limit+1,-1))));
-        long long ans = solve(zero,one,2,0,limit,dp);
+        vector<vector<vector<int>>> dp(zero+1,vector<vector<int>>(one+1,vector<int>(3,-1)));
+        long long ans = solve(zero,one,2,limit,dp);
 
         return ans%mod;
         
