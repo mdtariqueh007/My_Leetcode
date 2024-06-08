62d1b047698eaf0caae9d17c32ce370e04002acc
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-06-09 00:39:20 +0530

    Time: 2 ms (50.00%) | Memory: 7.8 MB (50.00%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
+        string res;
 
-        int n = s.length();
-
-        vector<int> marked(n,0);
-
-        for(int i = 0;i<n;i++){
-            if(s[i]>='0' && s[i]<='9'){
-                marked[i] = 1;
-                for(int j = i-1;j>=0;j--){
-                    if(s[j]>='a' && s[j]<='z' && !marked[j]){
-                        marked[j] = 1;
-                        break;
-                    }
-                }
+        for(char c : s){
+            if(isdigit(c)){
+                res.pop_back();
+            }
+            else{
+                res.push_back(c);
             }
         }
 
-        string ans = "";
-
-        for(int i = 0;i<n;i++){
-            if(!marked[i]){
-                ans += s[i];
-            }
-        }
-
-
-        return ans;
-        
+        return res;
     }
 };
