76169ac94536ed922144d61cb2818bc3817ead8b
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-06-14 01:20:39 +0530

    Time: 2 ms (99.06%) | Memory: 42.3 MB (97.82%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left<=right){
+            char c1 = s.charAt(left);
+            char c2 = s.charAt(right);
+
+            if(!Character.isLetterOrDigit(c1)){
+                left++;
+            }
+            else if(!Character.isLetterOrDigit(c2)){
+                right--;
+            }
+            else if(Character.toLowerCase(c1)!=Character.toLowerCase(c2)){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
