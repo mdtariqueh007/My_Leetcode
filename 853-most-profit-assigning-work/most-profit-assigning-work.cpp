ef1f4a9b79d6ba2e181d4d86721210bb79e3fb04
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-06-18 23:30:26 +0530

    Time: 2114 ms (5.02%) | Memory: 37.7 MB (98.20%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-
         int n = profit.size();
-
-        vector<pair<int,int>> v;
-
-        for(int i = 0;i<n;i++){
-            v.push_back({difficulty[i],profit[i]});
-        }
-
-        sort(v.begin(),v.end());
-
-        for(int i = 1;i<n;i++){
-            v[i].second = max(v[i].second,v[i-1].second);
-        }
-
-        int sum = 0;
 
         int m = worker.size();
 
-        for(int i = 0;i<m;i++){
-            int maxProfit = 0;
-            int low = 0;
-            int high = v.size() - 1;
-            while(low<=high){
-                int mid = low + (high - low)/2;
+        int maxProfit = 0;
 
-                if(v[mid].first<=worker[i]){
-                    maxProfit = max(maxProfit,v[mid].second);
-                    low = mid + 1;
-                }
-                else{
-                    high = mid - 1;
+        for(int i = 0;i<m;i++){
+            int curr = 0;
+            for(int j = 0;j<n;j++){
+                if(difficulty[j]<=worker[i]){
+                    curr = max(curr,profit[j]);
                 }
             }
-            sum += maxProfit;
+
+            maxProfit += curr;
         }
 
-        return sum;
-        
+        return maxProfit;
+
+
     }
 };
