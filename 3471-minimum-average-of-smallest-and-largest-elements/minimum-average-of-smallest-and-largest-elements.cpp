b497bde0137cfe1658a5ab19fcd07752228e6263
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-06-28 01:31:44 +0530

    Time: 3 ms (86.33%) | Memory: 25.3 MB (12.70%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
 
-        multiset<int> mt;
+        // multiset<int> mt;
 
-        for(int x : nums){
-            mt.insert(x);
-        }
+        // for(int x : nums){
+        //     mt.insert(x);
+        // }
+
+        sort(nums.begin(),nums.end());
 
         vector<double> averages;
 
         for(int i = 0;i<nums.size()/2;i++){
-            int mini = *mt.begin();
-            int maxi = *(--mt.end());
-
-            mt.erase(mt.begin());
-            mt.erase(--mt.end());
+            int mini = nums[i];
+            int maxi = nums[nums.size()- i - 1];
 
             averages.push_back((mini*1.0  + maxi * 1.0)/2.0);
             
