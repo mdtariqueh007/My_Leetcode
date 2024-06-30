6f26dd679b8e4f382c3a45c8a39bc4fb3add18b8
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-01 00:59:26 +0530

    Time: 75 ms (21.22%) | Memory: 31.3 MB (16.69%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
 
-        int i2, i3 ,i5;
-        i2 = i3 = i5 = 1;
-
-        vector<int> arr(n+1);
-
-        arr[1] = 1;
-
-        for(int i = 2;i<=n;i++){
-            int i2UglyNum = arr[i2] * 2;
-            int i3UglyNum = arr[i3] * 3;
-            int i5UglyNum = arr[i5] * 5;
-
-            int minUglyNum = min(i2UglyNum,min(i3UglyNum,i5UglyNum));
-
-            arr[i] = minUglyNum;
-
-            if(i2UglyNum==minUglyNum){
-                i2++;
-            }
-            if(i3UglyNum==minUglyNum){
-                i3++;
-            }
-            if(i5UglyNum==minUglyNum){
-                i5++;
-            }
-
+        if(n==1){
+            return 1;
         }
 
-        return arr[n];
+        int cnt = 0;
+        
+        set<long long> st;
+
+        long long curr = 1;
+
+        st.insert(curr);
+
+        while(cnt<n){
+            curr = *st.begin();
+            st.erase(st.begin());
+
+            st.insert(curr*2);
+            st.insert(curr*3);
+            st.insert(curr*5);
+
+            cnt++;
+        }
+
+        return curr;
+
+
         
     }
 };
