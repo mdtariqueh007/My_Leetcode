c571ff195392c8784946141613fe4cd845491ab9
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-11 18:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (39.15%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
+        int n = s.length();
 
         stack<int> st;
 
-        string temp = "";
-
-        for(int i = 0;i<s.length();i++){
+        unordered_map<int,int> mp;
+        for(int i = 0;i<n;i++){
             if(s[i]=='('){
-                st.push(temp.length());
+                st.push(i);
             }
             else if(s[i]==')'){
-                int ind = st.top();
+                int j = st.top();
                 st.pop();
-                reverse(temp.begin() + ind,temp.end());
-            }
-            else{
-                temp += s[i];
+
+                mp[i] = j;
+                mp[j] = i;
             }
         }
 
-        return temp;
-        
+        string res;
+
+        int flag = 1;
+
+        for(int i = 0;i<n;i+=flag){
+            if(s[i]=='(' || s[i]==')'){
+                i = mp[i];
+                flag = -flag;
+            }
+            else{
+                res.push_back(s[i]);
+            }
+        }
+
+        return res;
+
     }
 };
