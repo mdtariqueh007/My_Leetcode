629cdbaa27e73650d3bf43c1ad8f094a705f33a5
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-13 22:33:37 +0530

    Time: 13 ms (79.41%) | Memory: 17.8 MB (28.10%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    private:
+    bool checkCycle(int node,vector<int> adj[],vector<int>&vis,vector<int>&dfsVis){
+        vis[node] = 1;
+        dfsVis[node] = 1;
+
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(checkCycle(it,adj,vis,dfsVis)){
+                    return true;
+                }
+            }
+            else if(dfsVis[it]){
+                return true;
+            }
+        }
+
+        dfsVis[node] = 0;
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        vector<int> adj[n];
+
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> vis(n,0);
+        vector<int> dfsVis(n,0);
+
+        for(int i = 0;i<n;i++){
+            if(!vis[i]){
+                if(checkCycle(i,adj,vis,dfsVis)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
