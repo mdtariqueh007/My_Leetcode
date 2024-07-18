a7bf831123587c32efc45b92c8f287c9c413092f
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-18 19:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (41.03%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,46 +1,22 @@
 class Solution {
-    private:
-    
-    map<string,int> mp;
-    int solve(int ind,int prev,int curr,vector<int>&arr){ //curr==0 '>' else '<''
-        if(ind>=arr.size()){
-            return 0;
-        }
-        
-        string temp = to_string(ind) + "#" + to_string(prev) + "#" + to_string(curr);
-        
-        if(mp.find(temp)!=mp.end()){
-            return mp[temp];
-        }
-        
-        int take = 0, notTake = 0;
-        
-        notTake = solve(ind+1,prev,curr,arr);
-        
-        if(curr==0){
-            if(arr[ind]>prev){
-                take = 1 + solve(ind+1,arr[ind],1,arr);
-            }
-            // else{
-            //     take = solve(ind+1,prev,curr,arr);
-            // }
-        }
-        else{
-            if(arr[ind]<prev){
-                take = 1 + solve(ind+1,arr[ind],0,arr);
-            }
-            // else{
-            //     take = solve(ind+1,prev,curr,arr);
-            // }
-        }
-        
-        return mp[temp] = max(take,notTake);
-    }
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        mp.clear();
-        
-        
-        return max(solve(0,-1,0,nums),solve(0,1e9,1,nums));
+
+        if(nums.size()==1){
+            return 1;
+        }
+        int peak = 1, valley = 1;
+
+
+        for(int i = 1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                peak = valley + 1;
+            }
+            else if(nums[i]<nums[i-1]){
+                valley = peak + 1;
+            }
+        }
+
+        return max(peak,valley);
     }
 };
