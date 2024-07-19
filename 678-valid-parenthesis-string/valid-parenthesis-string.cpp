4adf5077f550ac8991229893aeae147d34c0c310
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-19 23:07:54 +0530

    Time: 3 ms (51.67%) | Memory: 7.4 MB (48.96%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,44 +1,28 @@
 class Solution {
-    private:
-    int dp[101][101];
-    bool solve(int ind,string&s,int cnt){
-        if(cnt<0){
-            return false;
-        }
-
-        if(ind==s.length()){
-            return cnt==0;
-        }
-
-        if(dp[ind][cnt]!=-1){
-            return dp[ind][cnt];
-        }
-
-        bool ans = false;
-
-        if(s[ind]=='('){
-            ans = solve(ind+1,s,cnt+1);
-        }
-        else if(s[ind]==')'){
-            ans = solve(ind+1,s,cnt-1);
-        }
-        else{
-            ans = solve(ind+1,s,cnt+1);
-            if(!ans){
-                ans = solve(ind+1,s,cnt-1);
-            }
-            if(!ans){
-                ans = solve(ind+1,s,cnt);
-            }
-        }
-
-        return dp[ind][cnt] = ans;
-        
-    }
 public:
     bool checkValidString(string s) {
-        memset(dp,-1,sizeof(dp));
-        
-        return solve(0,s,0);
+        int mini = 0;
+        int maxi = 0;
+
+        for(int i = 0;i<s.length();i++){
+            if(s[i]=='('){
+                mini = mini + 1;
+                maxi = maxi + 1;
+            }
+            else if(s[i]==')'){
+                mini = mini - 1;
+                maxi = maxi - 1;
+            }
+            else{
+                mini = mini - 1;
+                maxi = maxi + 1;
+            }
+
+            mini = max(0,mini);
+
+            if(maxi<0) return false;
+        }
+
+        return mini==0;
     }
 };
