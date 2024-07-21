d5190d9c85f756594a10905403e8ec1871dfb422
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-21 10:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (25.00%) - LeetSync

---
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string losingPlayer(int x, int y) {
+
+        int cnt = 0;
+
+        while(x>=1 && y>=4){
+            x = x - 1;
+            y = y - 4;
+
+            cnt++;
+        }
+
+        if(cnt%2==0){
+            return "Bob";
+        }
+        else{
+            return "Alice";
+        }
+        
+    }
+};
