e9b5ce3ae2ff62d8a90ae27a95361bb31279f29e
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-07-31 22:19:06 +0530

    Time: 19 ms (86.20%) | Memory: 11.9 MB (61.53%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int partitionString(string s) {
-        
-        unordered_set<char> st;
 
-        int n = s.size();
+        int n = s.length();
 
-        int cnt = 0;
+        vector<int> lastPos(26,-1);
+
+        int ans = 0;
+
+        int last = -1;
 
         for(int i = 0;i<n;i++){
-            if(st.find(s[i])!=st.end()){
-                cnt++;
-                st.clear();
+            if(lastPos[s[i]-'a']>=last){
+                ans++;
+                last = i;
             }
 
-            st.insert(s[i]);
+            lastPos[s[i]-'a'] = i;
         }
 
-        return cnt+1;
-
-
+        return ans;
+        
     }
 };
