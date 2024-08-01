fed4febc2e70610d6f3c81170b1eae3a35d51ec3
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-08-02 00:07:46 +0530

    Time: 163 ms (5.11%) | Memory: 106.4 MB (6.39%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,29 +1,53 @@
 class Solution {
-public:
-    long long countSubarrays(vector<int>& nums, long long k) {
+    private:
+    long long solve(int ind,vector<long long>&prefix,long long k){
+        long long ans = -1;
+        long long low = ind;
+        long long high = (int)prefix.size() - 1;
 
-        int n = nums.size();
+        while(low<=high){
+            int mid = low + (high - low)/2;
 
-        long long sum = 0;
+            long long length = (mid - ind + 1);
+            long long sum = prefix[mid] - (ind==0?0:prefix[ind-1]);
+            long long score = sum * length;
 
-        long long ans = 0;
-
-        int i = 0;
-        int j = 0;
-
-        while(j<n){
-            sum += nums[j];
-
-            while((sum*(j-i+1))>=k){
-                sum -= nums[i];
-                i++;
+            if(score>=k){
+                high = mid - 1;
             }
-
-            ans += (j - i + 1);
-            j++;
+            else{
+                ans = mid; 
+                low = mid + 1;
+            }
         }
 
         return ans;
-        
+    }
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
+
+        if(n==1 && nums[0]<k){
+            return 1;
+        }
+
+        vector<long long> prefix(n,0);
+
+        prefix[0] = nums[0];
+
+        for(int i = 1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        long long ans = 0;
+
+        for(int i = 0;i<n;i++){
+            long long right = solve(i,prefix,k);
+            if(right!=-1){
+                ans += (right - i + 1);
+            }
+        }
+
+        return ans;
     }
 };
