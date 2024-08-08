1011e27a3aed2746c8b79d45521c1ee8e7bcd366
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-08-08 18:34:47 +0530

    Time: 24 ms (72.76%) | Memory: 12.7 MB (63.94%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,42 +1,20 @@
 class Solution {
-    private:
-    int dp[100001][2];
-    int solve(string&s,int ind,int prev){
-        if(ind>=s.length()){
-            return 0;
-        }
-
-        if(dp[ind][prev]!=-1){
-            return dp[ind][prev];
-        }
-
-        int flip = 1e9;
-        int noFlip = 1e9;
-
-        if(s[ind]=='0'){
-            if(prev==1){
-                flip = 1 + solve(s,ind+1,1);
-            }
-            else{
-                flip = 1 + solve(s,ind+1,1);
-                noFlip = solve(s,ind+1,0);
-            }
-        }
-        else{
-            if(prev==1){
-                noFlip = solve(s,ind+1,1);
-            }
-            else{
-                flip = 1 + solve(s,ind+1,0);
-                noFlip = solve(s,ind+1,1);
-            }
-        }
-
-        return dp[ind][prev] = min(flip,noFlip);
-    }
 public:
     int minFlipsMonoIncr(string s) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s,0,0);
+
+        int cntOnes = 0;
+        int flips = 0;
+
+        for(char c : s){
+            if(c=='1'){
+                cntOnes++;
+            }
+            else{
+                flips = min(flips+1,cntOnes);
+            }
+        }
+
+        return flips;
+        
     }
 };
