00d2394301581785527198afcdd21d6523cabc5a
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-12-12 14:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (67.72%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    private:
+    int countNonBigger(int target, vector<vector<int>>&matrix){
+        int n = matrix.size();
+        int i = n - 1;
+        int j = 0;
+        int cnt = 0;
+
+        while(i>=0 && j<n){
+            if(matrix[i][j]>target){
+                i--;
+            }
+            else{
+                cnt += (i+1);
+                j++;
+            }
+        }
+
+        return cnt;
+    }
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+        int n = matrix.size();
+
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+
+        
+        while(low<=high){
+            int mid = low + (high - low)/2;
+
+            int cnt = countNonBigger(mid, matrix);
+
+            if(cnt<k){
+                
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return low;
+
+    }
+};
