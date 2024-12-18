329ea52f6e9435fecbf2a6ce7149a76e6ccb6d82
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-12-18 11:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (17.75%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
 
-        int n = prices.size();
 
-        vector<int> ans;
+        vector<int> ans = prices;
 
-        for(int i = 0;i<n;i++){
-            bool flag = false;
+        stack<int> st;
 
-            for(int j = i + 1;j<n;j++){
-                if(prices[j]<=prices[i]){
-                    flag = true;
-                    ans.push_back(prices[i] - prices[j]);
-                    break;
-                }
+        for(int i = 0;i<prices.size();i++){
+            while(!st.empty() && prices[i]<=prices[st.top()]){
+                ans[st.top()] -= prices[i];
+                st.pop();
             }
 
-            if(!flag){
-                ans.push_back(prices[i]);
-            }
+            st.push(i);
         }
 
         return ans;
+        
         
     }
 };
