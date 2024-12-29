a5127517659efbdf3f49457d0d1ad1e5a399e10f
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-12-29 19:43:08 +0530

    Time: 40 ms (90.75%) | Memory: 39.3 MB (73.97%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -2,6 +2,7 @@ class Solution {
     private:
     const int mod = 1e9 + 7;
     long long dp[1001][1001];
+    int freq[1001][26];
     long long solve(int i, int j, string& target, vector<string>&words){
         if(i==target.size()){
             return 1;
@@ -16,11 +17,15 @@ class Solution {
 
         long long cnt = 0;
 
-        for(int ind = 0;ind<words.size();ind++){
-            if(words[ind][j]==target[i]){
-                cnt += solve(i+1,j+1,target, words)%mod;
-            }
-        }
+        // for(int ind = 0;ind<words.size();ind++){
+        //     if(words[ind][j]==target[i]){
+        //         cnt += solve(i+1,j+1,target, words)%mod;
+        //     }
+        // }
+
+        int currChar = target[i] - 'a';
+
+        cnt += (freq[j][currChar] * solve(i+1,j+1, target, words))%mod;
 
         cnt += solve(i, j+1, target, words)%mod;
 
@@ -30,6 +35,14 @@ public:
     int numWays(vector<string>& words, string target) {
 
         memset(dp,-1,sizeof(dp));
+        memset(freq,0,sizeof(freq));
+
+        for(int i = 0;i<words.size();i++){
+            for(int j = 0;j<words[0].size();j++){
+                int currChar = words[i][j] - 'a';
+                freq[j][currChar]++;
+            }
+        }
 
         return solve(0,0,target, words);
         
