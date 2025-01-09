328a6a463235ec873477f42926cb63affea2733d
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-01-09 21:47:47 +0530

    Time: 30 ms (5.04%) | Memory: 63.6 MB (5.04%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -1,28 +1,75 @@
+class Node{
+    public:
+    Node* links[26] = {};
+    int cnt = 0;
+
+    bool contains(char c){
+        return links[c-'a']!=NULL;
+    }
+
+    Node* get(char c){
+        return links[c-'a'];
+    }
+
+    void put(char c, Node* node){
+        links[c-'a'] = node;
+    }
+
+    void incrementCnt(){
+        cnt++;
+    }
+
+    int getCnt(){
+        return cnt;
+    }
+};
+
+class Trie{
+    public:
+    Node* root;
+
+    Trie(){
+        root = new Node();
+    }
+
+    void insert(string& word){
+        Node* node = root;
+
+        for(char c: word){
+            if(!node->contains(c)){
+                node->put(c, new Node());
+            }
+
+            node = node->get(c);
+            node->incrementCnt();
+        }
+    }
+
+    int countWordsWithPrefix(string& prefix){
+        Node* node = root;
+
+        for(char c: prefix){
+            if(!node->contains(c)){
+                return 0;
+            }
+            node = node->get(c);
+        }
+
+        return node->getCnt();
+    }
+};
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-
-        int m = pref.length();
-
-        int ans = 0;
+        
+        Trie tr;
 
         for(string& s: words){
-            if(s.length()<m){
-                continue;
-            }
-
-            int i;
-
-            for(i = 0;i<m;i++){
-                if(s[i]!=pref[i]){
-                    break;
-                }
-            }
-
-            if(i==m){
-                ans++;
-            }
+            tr.insert(s);
         }
+
+        int ans = tr.countWordsWithPrefix(pref);
 
         return ans;
         
