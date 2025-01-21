bbc82ddaac2ac0b4437b62b9d6d4777ba3d30489
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-01-22 00:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (10.12%) - LeetSync

--- 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
@@ -4,11 +4,24 @@ public:
 
         int ans = 0;
 
+        int n = nums.size();
+
+        vector<int> prefix(n,0);
+
+        prefix[0] = nums[0];
+
+        for(int i = 1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
         for(int i = 0;i<nums.size();i++){
             int start = max(0, i - nums[i]);
 
-            for(int j = start;j<=i;j++){
-                ans += nums[j];
+            if(start==0){
+                ans += prefix[i];
+            }
+            else{
+                ans += (prefix[i] - prefix[start-1]);
             }
         }
 
