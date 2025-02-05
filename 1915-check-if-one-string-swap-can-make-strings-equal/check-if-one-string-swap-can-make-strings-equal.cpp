b95a2800b6d9cc874c0b0e65d0270139d983dddb
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-02-05 10:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (61.87%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-
-        vector<int> freq1(26,0), freq2(26,0);
+        
+        int firstInd = 0, secondInd = 0;
 
         int diff = 0;
 
         for(int i = 0;i<s1.size();i++){
-            freq1[s1[i] - 'a']++;
-            freq2[s2[i] - 'a']++;
-
             if(s1[i]!=s2[i]){
                 diff++;
-            }
+            
 
             if(diff>2){
+                
                 return false;
+            }
+            else if(diff==1){
+                firstInd = i;
+            }
+            else{
+                secondInd = i;
+            }
             }
         }
 
-        for(int i = 0;i<26;i++){
-            if(freq1[i]!=freq2[i]){
-                return false;
-            }
-        }
+        return (s1[firstInd]==s2[secondInd] && s1[secondInd]==s2[firstInd]);
 
-        return true;
-        
     }
 };
