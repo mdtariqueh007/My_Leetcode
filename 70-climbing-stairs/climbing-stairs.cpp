922199d63d8bb34f84769fbd836719316c180972
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 00:55:40 +0530

    Time: 2 ms (5.30%) | Memory: 8.6 MB (17.15%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -21,7 +21,23 @@ class Solution {
     }
 public:
     int climbStairs(int n) {
-        memset(memo, -1 , sizeof(memo));
-        return solve(0, n);
+        // memset(memo, -1 , sizeof(memo));
+        // return solve(0, n);
+
+
+        if(n==1 || n==2){
+            return n;
+        }
+
+        vector<int> dp(n+1, 0);
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
