52f3fc8f7da1db3f21603fa31e20f644c5ad6fea
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 01:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.86%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -10,17 +10,26 @@ public:
             return 1;
         }
 
-        vector<int> dp(n+1,0);
+        // vector<int> dp(n+1,0);
 
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 1;
+
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev1 = 1;
 
         for(int i = 3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            // dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int curr = prev1 + prev2 + prev3;
+
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
         
     }
 };
