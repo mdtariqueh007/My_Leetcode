fc2d3af2aa01a7d45d2b9da96ba38ce47f98a5bc
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 01:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,24 @@
 class Solution {
-    private:
-    int dp[101];
-    int solve(int curr, vector<int>&nums){
-        if(curr>=nums.size()){
-            return 0;
-        }
-
-        if(dp[curr]!=-1){
-            return dp[curr];
-        }
-
-        int rob = nums[curr] + solve(curr+2, nums);
-        int dontRob = solve(curr+1, nums);
-
-        return dp[curr] = max(rob, dontRob);
-    }
+    
 public:
     int rob(vector<int>& nums) {
 
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
 
-        return solve(0, nums);
+        // return solve(0, nums);
+
+        int n = nums.size();
+
+        vector<int> dp(n+2, 0);
+
+        for(int i = n-1;i>=0;i--){
+            int rob = nums[i] + dp[i+2];
+            int dontRob = dp[i+1];
+
+            dp[i] = max(rob, dontRob);
+        }
+
+        return dp[0];
         
     }
 };
