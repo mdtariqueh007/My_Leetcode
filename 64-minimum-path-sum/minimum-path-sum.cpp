47341e4fd40850c033d9372ffdb904d37cefe6e0
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 16:11:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (85.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -10,27 +10,26 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        // vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
-        dp[m-1][n-1] = grid[m-1][n-1];
+        vector<int> dp(n+1);
 
-        for(int i = m - 2;i>=0;i--){
-            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
-        }
+        dp[n-1] = grid[m-1][n-1];
+
         for(int j = n - 2;j>=0;j--){
-            dp[m-1][j] = grid[m-1][j] + dp[m-1][j+1];
+            dp[j] = grid[m-1][j] + dp[j+1];
         }
 
         for(int i = m-2;i>=0;i--){
-            for(int j = n-2;j>=0;j--){
-                int moveRight = grid[i][j] + dp[i][j+1];
-                int moveDown = grid[i][j] + dp[i+1][j];
+            for(int j = n-1;j>=0;j--){
+                int moveRight = grid[i][j] + (j+1<n?dp[j+1]:1e9);
+                int moveDown = grid[i][j] + dp[j];
 
-                dp[i][j] = min(moveRight, moveDown);
+                dp[j] = min(moveRight, moveDown);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
 
 
         
