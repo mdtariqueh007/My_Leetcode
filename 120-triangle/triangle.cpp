e6593ca7e65a824b89e0e4e30f5c734d5dea496f
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 17:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (5.74%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -18,9 +18,32 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
 
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
 
-        return solve(0, 0, triangle);
+        // return solve(0, 0, triangle);
+
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+        for(int j = n - 1;j>=0;j--){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        // for(int i = n - 2;i>=0;i--){
+        //     dp[i][n-1] = triangle[i][n-1] + dp[i+1][n-1];
+        // }
+
+        for(int i = n-2;i>=0;i--){
+            for(int j = i;j>=0;j--){
+                int moveDown = triangle[i][j] + dp[i+1][j];
+                int moveDiag = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(moveDown, moveDiag);
+            }
+        }
+
+        return dp[0][0];
         
     }
 };
