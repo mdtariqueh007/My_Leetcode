fbaa60435abc1564a225f6c8bc7a4894f6d43423
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 23:04:19 +0530

    Time: 79 ms (51.59%) | Memory: 9.2 MB (94.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,58 +1,29 @@
 class Solution {
     private:
-    vector<int> helper(string s){
-        int n = s.length();
-
-        vector<int> lps(n,0);
-
-        int c = 0;
-        int r = 0;
-
-        for(int i = 1;i<s.length()-1;i++){
-            int mirr = c - (i - c);
-            if(i<r){
-                lps[i] = min(lps[mirr],r-i);
+    bool isPal(int i, int j, string&s){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
             }
-            while((i+1+lps[i])<n && (i-1-lps[i])>=0 && s[i+1+lps[i]]==s[i-1-lps[i]]){
-                lps[i]++;
-            }
-            if(i+lps[i]>r){
-                c = i;
-                r = i + lps[i];
-            }
+
+            i++;
+            j--;
         }
 
-        return lps;
+        return true;
     }
 public:
     string longestPalindrome(string s) {
 
-        string st = "$";
-
-        for(int i = 0;i<s.length();i++){
-            st += "#";
-            st += s[i];
-            
-        }
-        st += "#";
-        st += "$";
-        
-        vector<int> lps = helper(st);
-
-        int maxlen = 0;
-        int ind = 0;
-        for(int i = 0;i<lps.size();i++){
-            if(lps[i]>maxlen){
-                maxlen = lps[i];
-                ind = i;
+        for(int len = s.size();len>0;len--){
+            for(int start = 0;start<=s.size()-len;start++){
+                if(isPal(start, start+len-1, s)){
+                    return s.substr(start, len);
+                }
             }
         }
 
-        int startInd = ind - maxlen + 1;
-        int actInd = (startInd - 2)/2;
-
-        return s.substr(actInd,maxlen);
-
-
+        return "";
+        
     }
 };
