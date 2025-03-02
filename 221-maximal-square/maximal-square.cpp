4820f75e8dd6867f3e64e2fb34c8a01adba43f1d
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 22:56:13 +0530

    Time: 5 ms (78.02%) | Memory: 30.7 MB (81.49%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -5,33 +5,51 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n,0));
+        // vector<vector<int>> dp(m, vector<int>(n,0));
+
+        vector<int> prev(n,0);
 
         int maxi = 0;
 
-        for(int i = 0;i<m;i++){
-            if(matrix[i][0]=='1'){
-                dp[i][0] = 1;
-                maxi = 1;
-            }
+        // for(int i = 0;i<m;i++){
+        //     if(matrix[i][0]=='1'){
+        //         dp[i][0] = 1;
+        //         maxi = 1;
+        //     }
+        // }
+
+        if(matrix[0][0]=='1'){
+            prev[0] = 1;
+            maxi = 1;
         }
 
-        for(int j = 0;j<n;j++){
+        for(int j = 1;j<n;j++){
             if(matrix[0][j]=='1'){
-                dp[0][j] = 1;
+                prev[j] = 1;
                 maxi = 1;
             }
         }
 
         for(int i = 1;i<m;i++){
+            vector<int> curr(n,0);
+            if(matrix[i][0]=='1'){
+                curr[0] = 1;
+                maxi = max(maxi, curr[0]);
+            }
             for(int j = 1;j<n;j++){
 
-                if(matrix[i][j]=='0') continue;
+                if(matrix[i][j]=='1'){
 
-                dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                curr[j] = 1 + min(prev[j], min(prev[j-1], curr[j-1]));
 
-                maxi = max(maxi, dp[i][j]);
+                maxi = max(maxi, curr[j]);
+                }
+                else{
+                    curr[j] = 0;
+                }
             }
+
+            prev = curr;
         }
 
         return maxi * maxi;
