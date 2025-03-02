3cda4f14a2a0b550038be573291a09d7b778b77a
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-02 18:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (67.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,20 +1,5 @@
 class Solution {
-    private:
-    int dp[201][201];
-    int solve(int i, int j, vector<vector<int>>&triangle){
-        if(i==triangle.size()-1){
-            return triangle[i][j];
-        }
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        int moveDown = triangle[i][j] + solve(i+1, j, triangle);
-        int moveDiag = triangle[i][j] + solve(i+1, j+1, triangle);
-
-        return dp[i][j] = min(moveDown, moveDiag);
-    }
+    
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
 
@@ -24,26 +9,43 @@ public:
 
         int n = triangle.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        // vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+        vector<int> dp(n+1, 0);
 
         for(int j = n - 1;j>=0;j--){
-            dp[n-1][j] = triangle[n-1][j];
+            dp[j] = triangle[n-1][j];
         }
+
+        // for(auto it: dp){
+        //     cout<<it<<" ";
+        // }
+        // cout<<"\n";
 
         // for(int i = n - 2;i>=0;i--){
         //     dp[i][n-1] = triangle[i][n-1] + dp[i+1][n-1];
         // }
 
         for(int i = n-2;i>=0;i--){
-            for(int j = i;j>=0;j--){
-                int moveDown = triangle[i][j] + dp[i+1][j];
-                int moveDiag = triangle[i][j] + dp[i+1][j+1];
+            for(int j = 0;j<=i;j++){
+                int moveDown = triangle[i][j] + dp[j];
+                int moveDiag = triangle[i][j] + dp[j+1];
 
-                dp[i][j] = min(moveDown, moveDiag);
+                dp[j] = min(moveDown, moveDiag);
             }
+
+        //     for(auto it: dp){
+        //     cout<<it<<" ";
+        // }
+        // cout<<"\n";
         }
 
-        return dp[0][0];
+        // for(auto it: dp){
+        //     cout<<it<<" ";
+        // }
+        // cout<<"\n";
+
+        return dp[0];
         
     }
 };
