45d0b4c3102748ceb7d350d567851ace516c8406
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-03 23:20:20 +0530

    Time: 29 ms (64.39%) | Memory: 13 MB (81.90%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -32,6 +32,26 @@ public:
         memset(dp, -1, sizeof(dp));
 
         return solve(0,0,s,t);
+
+        // int m = s.size(), n = t.size();
+
+
+        // vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        // dp[m][n] = 1;
+
+        // for(int i = m - 1;i>=0;i--){
+        //     for(int j = n - 1;j>=0;j--){
+        //         if(s[i]==t[j]){
+        //             dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+        //         }
+        //         else{
+        //             dp[i][j] = dp[i+1][j];
+        //         }
+        //     }
+        // }
+
+        // return dp[0][0];
         
     }
 };
