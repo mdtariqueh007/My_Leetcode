1b83e104f157847908ad0412b3529d30f448b34c
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-04 00:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,52 +1,24 @@
 class Solution {
-    // private:
-    // int dp[2502][2502];
-    // int solve(int curr, int prev, vector<int>&nums){
-    //     if(curr==nums.size()){
-    //         return 0;
-    //     }
-
-    //     if(dp[curr][prev+1]!=-1){
-    //         return dp[curr][prev+1];
-    //     }
-
-    //     int take = 0, notTake = 0;
-
-    //     if(prev==-1 || nums[curr]>nums[prev]){
-
-    //         take = 1 + solve(curr+1, curr, nums);
-
-    //     }
-
-    //     notTake = solve(curr+1, prev, nums);
-
-    //     return dp[curr][prev+1] = max(take, notTake);
-    // }
 public:
     int lengthOfLIS(vector<int>& nums) {
 
-        // memset(dp, -1, sizeof(dp));
-
-        // return solve(0, -1, nums);
-
         int n = nums.size();
 
-        vector<int> dp(n, 1);
+        vector<int> temp;
 
-        int maxi = 1;
+        temp.push_back(nums[0]);
 
         for(int i = 1;i<n;i++){
-            for(int j = 0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                    maxi = max(maxi, dp[i]);
-                }
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i]; 
             }
         }
 
-        return maxi;
-
-
+        return temp.size();
         
     }
 };
