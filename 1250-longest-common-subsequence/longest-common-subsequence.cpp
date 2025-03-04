1d7e3da4186103c068b1e51215249c0533cf5ded
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-05 02:24:22 +0530

    Time: 11 ms (97.52%) | Memory: 9.1 MB (96.22%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -29,20 +29,24 @@ public:
 
         int n = text1.size(), m = text2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        vector<int> dp(m+1, 0), dpNext(m+1,0);
 
         for(int i = n - 1;i>=0;i--){
             for(int j = m - 1;j>=0;j--){
                 if(text1[i]==text2[j]){
-                    dp[i][j] = 1 + dp[i+1][j+1];
+                    dp[j] = 1 + dpNext[j+1];
                 }
                 else{
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                    dp[j] = max(dpNext[j], dp[j+1]);
                 }
             }
+
+            dpNext = dp;
         }
 
-        return dp[0][0];
+        return dp[0];
         
     }
 };
