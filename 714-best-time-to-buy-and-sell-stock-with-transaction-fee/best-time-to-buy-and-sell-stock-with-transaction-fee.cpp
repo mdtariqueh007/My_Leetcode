4d547e4e64c2493a4caec0caa9f7cdc6bce6ec68
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-05 13:20:28 +0530

    Time: 102 ms (38.57%) | Memory: 95.2 MB (34.35%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,38 +1,56 @@
 class Solution {
-    private:
-    int dp[50001][2];
-    int solve(int day, int buy, vector<int>&prices, int fee){
-        if(day>=prices.size()){
-            return 0;
-        }
+    // private:
+    // int dp[50001][2];
+    // int solve(int day, int buy, vector<int>&prices, int fee){
+    //     if(day>=prices.size()){
+    //         return 0;
+    //     }
 
-        if(dp[day][buy]!=-1){
-            return dp[day][buy];
-        }
+    //     if(dp[day][buy]!=-1){
+    //         return dp[day][buy];
+    //     }
 
-        if(buy){
+    //     if(buy){
 
-            int dontBuyStock = solve(day+1, buy, prices, fee);
-            int buyStock = -prices[day] + solve(day+1, false, prices, fee);
+    //         int dontBuyStock = solve(day+1, buy, prices, fee);
+    //         int buyStock = -prices[day] + solve(day+1, false, prices, fee);
 
-            return dp[day][buy] = max(dontBuyStock, buyStock);
+    //         return dp[day][buy] = max(dontBuyStock, buyStock);
 
-        }
-        else{
-            int dontSellStock = solve(day+1, buy, prices, fee);
-            int sellStock = prices[day] - fee + solve(day+1, true, prices, fee);
+    //     }
+    //     else{
+    //         int dontSellStock = solve(day+1, buy, prices, fee);
+    //         int sellStock = prices[day] - fee + solve(day+1, true, prices, fee);
 
-            return dp[day][buy] = max(dontSellStock, sellStock);
-        }
+    //         return dp[day][buy] = max(dontSellStock, sellStock);
+    //     }
 
 
-    }
+    // }
 public:
     int maxProfit(vector<int>& prices, int fee) {
 
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
 
-        return solve(0, true, prices, fee);
+        // return solve(0, true, prices, fee);
+
+
+        int n = prices.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+
+        for(int i = n - 1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    dp[i][buy] = max(dp[i+1][buy], -prices[i] + dp[i+1][1-buy]);
+                }
+                else{
+                    dp[i][buy] = max(dp[i+1][buy], prices[i]-fee + dp[i+1][1-buy]);
+                }
+            }
+        }
+
+        return dp[0][1];
         
     }
 };
