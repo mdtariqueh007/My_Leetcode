6434c5d828c919c5c04d5ccb9529d64df20a9bb2
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-05 13:08:14 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (85.12%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,19 +4,22 @@ public:
 
         int n = prices.size();
 
-        vector<int> reset(n, 0), held(n, 0), sold(n, 0);
+        // vector<int> reset(n, 0), held(n, 0), sold(n, 0);
 
-        reset[0] = 0;
-        held[0] = -prices[0];
-        sold[0] = -1e9;
+        // reset[0] = 0;
+        // held[0] = -prices[0];
+        // sold[0] = -1e9;
+
+        int reset = 0, held = -prices[0], sold = -1e9;
 
         for(int i = 1;i<n;i++){
-            sold[i] = held[i-1] + prices[i];
-            held[i] = max(held[i-1], reset[i-1] - prices[i]);
-            reset[i] = max(reset[i-1], sold[i-1]);
+            int prevSold = sold;
+            sold = held + prices[i];
+            held = max(held, reset - prices[i]);
+            reset = max(reset, prevSold);
         }
 
-        return max(sold.back(), reset.back());
+        return max(sold, reset);
 
 
 
