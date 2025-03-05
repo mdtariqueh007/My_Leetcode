9abb55a3e9e4ab55300408195a75fb0195782ea9
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-05 17:09:55 +0530

    Time: 1 ms (86.05%) | Memory: 14.4 MB (85.09%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,61 +1,19 @@
 class Solution {
-    // private:
-    // int dp[1001][2][101];
-    // int solve(int day, bool buy, int cap, vector<int>& prices){
-    //     if(cap==0){
-    //         return 0;
-    //     }
-    //     if(day==prices.size()){
-    //         return 0;
-    //     }
-
-    //     if(dp[day][buy][cap]!=-1){
-    //         return dp[day][buy][cap];
-    //     }
-
-    //     if(buy){
-    //         int dontBuy = solve(day+1, buy, cap, prices);
-    //         int buyStock = -prices[day] + solve(day+1, false, cap, prices);
-
-    //         return dp[day][buy][cap] = max(dontBuy, buyStock);
-    //     }
-    //     else{
-    //         int dontSell = solve(day+1, buy, cap, prices);
-    //         int sellStock = prices[day] + solve(day+1, true, cap-1, prices);
-
-    //         return dp[day][buy][cap] = max(dontSell, sellStock);
-    //     }
-
-
-    // }
 public:
     int maxProfit(int k, vector<int>& prices) {
 
-        // memset(dp, -1, sizeof(dp));
-
-        // return solve(0, true, k, prices);
-
         int n = prices.size();
 
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+        vector<int> buy(k+1, -1e9), sell(k+1, 0);
 
-        vector<vector<vector<int>>> dp(2, vector<vector<int>>(2, vector<int>(k+1,0)));
-
-
-        for(int i = n - 1;i>=0;i--){
-            for(int buy = 0;buy<=1;buy++){
-                for(int cap=1;cap<=k;cap++){
-                    if(buy){
-                        dp[i%2][buy][cap] = max(dp[(i+1)%2][buy][cap], -prices[i]+dp[(i+1)%2][1-buy][cap]);
-                    }
-                    else{
-                        dp[i%2][buy][cap] = max(dp[(i+1)%2][buy][cap], prices[i]+dp[(i+1)%2][1-buy][cap-1]);
-                    }
-                }
+        for(int i = 0;i<n;i++){
+            for(int j = 1;j<=k;j++){
+                buy[j] = max(buy[j], -prices[i] + sell[j-1]);
+                sell[j] = max(sell[j], prices[i] + buy[j]);
             }
         }
 
-        return dp[0][1][k];
+        return sell[k];
         
     }
 };
