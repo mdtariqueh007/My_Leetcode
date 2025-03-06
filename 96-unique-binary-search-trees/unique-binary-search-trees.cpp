4595c47379a93f4e459d5f9e60b1211f3290bcb1
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-06 12:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.25%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int numTrees(int n) {
 
-        vector<int> g(n+1);
+        long long ans = 1;
 
-        g[0] = 1;
-        g[1] = 1;
-
-        for(int i = 2;i<=n;i++){
-            for(int j = 1;j<=i;j++){
-                g[i] += g[j-1] * g[i-j];
-            }
+        for(int i = 1;i<=n;i++){
+            ans = ans * (4 * i - 2)/(i + 1);
         }
 
-        return g[n];
+        return ans;
+
         
     }
 };
