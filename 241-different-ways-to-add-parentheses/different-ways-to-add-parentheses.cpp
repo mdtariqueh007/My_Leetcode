435c12ca3b5a65de74c15b8da9154ca024ae5241
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-13 00:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (93.71%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,32 +1,36 @@
 class Solution {
     private:
-    vector<int> solve(string expression){
+    
+    vector<int> solve(string& expression, vector<vector<vector<int>>>&dp, int start, int end){
+
+        if(!dp[start][end].empty()){
+            return dp[start][end];
+        }
+
         vector<int> res;
 
-        if(expression.size()==0){
+        if(start==end){
+            res.push_back(expression[start] - '0');
             return res;
         }
 
-        if(expression.size()==1){
-            res.push_back(stoi(expression));
+        if(end-start==1 && isdigit(expression[start])){
+            int tens = expression[start] - '0';
+            int ones = expression[end] - '0';
+
+            res.push_back(tens*10 + ones);
             return res;
         }
 
-        if(expression.size()==2 && isdigit(expression[0])){
-            res.push_back(stoi(expression));
-
-            return res;
-        }
-
-        for(int i = 0;i<expression.size();i++){
+        for(int i = start;i<=end;i++){
             char curr = expression[i];
 
             if(isdigit(curr)){
                 continue;
             }
 
-            vector<int> leftRes = solve(expression.substr(0,i));
-            vector<int> rightRes = solve(expression.substr(i+1));
+            vector<int> leftRes = solve(expression, dp, start, i - 1);
+            vector<int> rightRes = solve(expression, dp, i+1, end);
 
             for(int lVal: leftRes){
                 for(int rVal: rightRes){
@@ -47,12 +51,16 @@ class Solution {
             }
         }
 
-        return res;
+        return dp[start][end] = res;
     }
 public:
     vector<int> diffWaysToCompute(string expression) {
 
-        return solve(expression);
+        int n = expression.size();
+
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n));
+
+        return solve(expression, dp, 0, expression.size() - 1);
 
         
         
