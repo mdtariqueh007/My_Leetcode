321ddd4e929c6d2343b674a7b7ca519cbf6b73ec
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-19 00:19:02 +0530

    Time: 187 ms (32.36%) | Memory: 16 MB (60.30%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,85 +1,43 @@
 class Solution {
-
 public:
     int countPalindromicSubsequence(string s) {
 
-        int n = s.length();
-        
-        vector<pair<int,int>> occ(26,{-1,-1});
+        unordered_set<char> letters;
 
-        for(int i = 0;i<n;i++){
-            if(occ[s[i]-'a'].first==-1){
-                occ[s[i]-'a'].first = i;
-            }
-            else{
-                occ[s[i]-'a'].second = i;
-            }
+        for(char c: s){
+            letters.insert(c);
         }
 
-        // vector<int> pre(n,0);
+        int ans = 0;
 
-        // pre[0] = 1;
+        for(char c: letters){
 
-        // unordered_set<char> st;
+            int first = -1;
+            int last = 0;
 
-        // st.insert(s[0]);
-        // for(int i = 1;i<n;i++){
-        //     if(st.find(s[i])!=st.end()){
-        //         pre[i] = pre[i-1];
-        //     }
-        //     else{
-        //         pre[i] =pre[i-1] + 1;
-        //         st.insert(s[i]);
-        //     }
-        // }
+            for(int i = 0;i<s.size();i++){
+                if(s[i]==c){
+                    if(first==-1){
+                        first = i;
+                    }
 
-        // for(int it : pre){
-        //     cout<<it<<" ";
-        // }
-
-        // cout<<"\n";
-
-       
-
-        int res = 0;
-
-        vector<int> visited(26,0);
-
-        for(int i = 0;i<n;i++){
-            if(occ[s[i]-'a'].second!=-1 && !visited[s[i]-'a']){
-                int firstOcc = occ[s[i]-'a'].first;
-                int lastOcc = occ[s[i]-'a'].second;
-
-                // int countDistinct = 0;
-                // if(firstOcc==0){
-                //     countDistinct = pre[lastOcc];
-                // }
-                // else{
-                //     countDistinct = pre[lastOcc] - pre[firstOcc-1];
-                // }
-
-
-                // res += countDistinct;
-
-                visited[s[i]-'a'] = 1;
-
-                unordered_set<char> dis;
-
-                for(int ind = firstOcc+1;ind<=lastOcc-1;ind++){
-                    dis.insert(s[ind]);
+                    last = i;
                 }
-
-                res += dis.size();
-                 
             }
-           
+
+            unordered_set<char> st;
+
+            for(int i = first+1;i<=last-1;i++){
+                st.insert(s[i]);
+            }
+
+            ans += st.size();
+
         }
 
-         
-        return res;
-
-
-
+        return ans;
         
     }
+
+
 };
