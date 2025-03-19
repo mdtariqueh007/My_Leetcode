55e1a1916a35fded83a435308ddb93565e867cf6
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-20 00:14:40 +0530

    Time: 19 ms (95.74%) | Memory: 17.7 MB (44.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,48 +1,92 @@
-class Solution {
+class DSU{
     private:
-    void bfs(int i,int j,vector<vector<bool>>&vis,vector<vector<char>>&grid){
-        vis[i][j] = true;
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        int n = grid.size();
-        int m = grid[0].size();
+    vector<int> parent;
+    vector<int> size;
+    
+    public:
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n, 1);
 
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            int dx[4] = {0,0,-1,1};
-            int dy[4] = {-1,1,0,0};
-            for(int i = 0;i<4;i++){
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
-                   q.push({nrow,ncol});
-                    vis[nrow][ncol] = true;
-                }
-            }
+        for(int i = 0;i<n;i++){
+            parent[i] = i;
         }
     }
+
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu==pv){
+            return;
+        }
+
+        if(size[pu]>=size[pv]){
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else{
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+    }
+
+    
+};
+
+class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
 
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vector<vector<bool>> vis(n,vector<bool>(m,0));
+        DSU ds(m*n);
 
-        int count = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    count++;
-                    bfs(i,j,vis,grid);
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    int parentVal = i * n + j;
+
+                    for(int k = 0;k<4;k++){
+                        int ni = i + dx[k];
+                        int nj = j + dy[k];
+
+                        if(ni>=0 && nj>=0 && ni<m && nj<n && grid[ni][nj]=='1'){
+                            int newVal = ni * n + nj;
+
+                            ds.Union(parentVal, newVal);
+                        }
+                    }
                 }
             }
         }
 
-        return count;
+        int ans = 0;
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]=='1' && ds.find(i * n + j)==(i * n + j)){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+
+        
+
+        return ans;
         
     }
 };
