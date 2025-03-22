f91faf2ee09b10cbf9366e43dfe1ff12721df1a9
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-22 08:20:56 +0530

    Time: 31 ms (85.23%) | Memory: 124.1 MB (88.21%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,47 +1,89 @@
-class Solution {
+class DSU{
     private:
-    void dfs(int node,vector<int> adj[],vector<int>&vis,int &n,double &m){
-        vis[node] = 1;
+    vector<int> parent, size, edges;
 
-        n++;
-        m += adj[node].size()/2.0;
-
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                dfs(it,adj,vis,n,m);
-            }
-        }
-    }
-public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        
-        vector<int> adj[n];
-
-        for(auto it : edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-
-        int res = 0;
-
-        vector<int> vis(n,0);
+    public:
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n);
+        edges.resize(n);
 
         for(int i = 0;i<n;i++){
-            if(!vis[i]){
-                int n = 0;
-                double m = 0;
-                dfs(i,adj,vis,n,m);
-                // cout<<n<<" "<<m<<"\n";
-                int edges = (n*(n-1))/2;
+            parent[i] = i;
+            size[i] = 1;
+            edges[i] = 0;
+        }
+    }
 
-                if(edges==m){
-                    res++;
+    int find(int u){
+        if(parent[u]==u){
+            return u;
+        }
+
+        return parent[u] = find(parent[u]);
+    }
+
+    void Union(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu==pv){
+            edges[pu]++;
+            return;
+        }
+
+        if(size[pu]>=size[pv]){
+            parent[pv] = pu;
+            size[pu] += size[pv];
+            edges[pu] += edges[pv] + 1;
+        }
+        else{
+            parent[pu] = pv;
+            size[pv] += size[pu];
+            edges[pv] += edges[pu] + 1;
+        }
+    }
+
+    int getEdges(int u){
+        return edges[u];
+    }
+
+    int getNodes(int u){
+        return size[u];
+    }
+};
+
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+
+        DSU ds(n);
+
+        for(auto it: edges){
+            ds.Union(it[0], it[1]);
+        }
+
+        int ans = 0;
+
+        for(int i = 0;i<n;i++){
+            if(ds.find(i)==i){
+                int nodeCount = ds.getNodes(i);
+                int edgesCount = ds.getEdges(i);
+
+                if(i==1){
+                    cout<<edgesCount<<" "<<nodeCount<<"\n";
                 }
-                
+
+                if(edgesCount == (nodeCount * (nodeCount - 1))/2){
+                    if(i==1){
+                        cout<<"YES\n";
+                    }
+                    ans++;
+                }
             }
         }
 
-        return res;
-
+        return ans;
+        
     }
 };
