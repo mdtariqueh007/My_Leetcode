17232273ae8f002e501ce767641962dc75678992
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-23 22:57:47 +0530

    Time: 87 ms (36.77%) | Memory: 49.7 MB (42.07%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.cpp
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -9,49 +9,11 @@ public:
         int sz1 = k/2;
         int sz2 = k&1?k/2+1:k/2;
 
-       
-
-        for(int i = 0;i<k;i++){
-            int val = nums[i];
-
-            if(low.empty() || val<=*low.rbegin()){
-                low.insert(val);
-            }
-            else{
-                high.insert(val);
-            }
-
-            while(low.size()>sz1){
-                int x = *low.rbegin();
-                low.erase(prev(low.end()));
-                high.insert(x);
-            }
-
-            while(high.size()>sz2){
-                int x = *high.begin();
-                high.erase(high.begin());
-                low.insert(x);
-            }
-
-        }
-
-        
-
-        
         vector<double> ans;
 
         double median;
 
-        if(k&1){
-            median = *high.begin();
-        }
-        else{
-            median = (*high.begin()*1.0 + *low.rbegin()*1.0)*1.0/2.0;
-        }
-
-        ans.push_back(median);
-
-        for(int i = k;i<n;i++){
+        for(int i = 0;i<n;i++){
             int val = nums[i];
 
             if(low.empty() || val<=*low.rbegin()){
@@ -61,13 +23,16 @@ public:
                 high.insert(val);
             }
 
-            int toRemove = nums[i-k];
+            if(i>=k){
 
-            if(low.count(toRemove)){
-                low.erase(low.find(toRemove));
-            }
-            else{
-                high.erase(high.find(toRemove));
+                int toRemove = nums[i-k];
+
+                if(low.count(toRemove)){
+                    low.erase(low.find(toRemove));
+                }
+                else{
+                    high.erase(high.find(toRemove));
+                }
             }
 
              while(low.size()>sz1){
@@ -82,16 +47,18 @@ public:
                 low.insert(x);
             }
 
+            if(i>=k-1){
             
-            if(k&1){
-                median = *high.begin();
-            }
-            else{
-                median = (*high.begin()*1.0 + *low.rbegin()*1.0)*1.0/2.0;
-            }
+                if(k&1){
+                    median = *high.begin();
+                }
+                else{
+                    median = (*high.begin()*1.0 + *low.rbegin()*1.0)*1.0/2.0;
+                }
 
-            ans.push_back(median);
+                ans.push_back(median);
 
+            }
 
         }
 
