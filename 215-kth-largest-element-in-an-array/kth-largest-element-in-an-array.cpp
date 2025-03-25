1595b8277112ad1deb026e575a919d5141cd653a
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-25 13:00:32 +0530

    Time: 35 ms (47.50%) | Memory: 61.6 MB (27.24%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -5,14 +5,10 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
 
         for(int x: nums){
-            if(pq.size()<k){
-                pq.push(x);
-            }
-            else{
-                if(pq.top()<x){
-                    pq.pop();
-                    pq.push(x);
-                }
+            pq.push(x);
+
+            if(pq.size()>k){
+                pq.pop();
             }
         }
 
