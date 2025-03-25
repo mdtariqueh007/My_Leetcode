d7346f13a6e57484ee88aba8c84224035de31fa3
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-25 12:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (92.12%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,13 +4,38 @@ public:
 
         int n = nums.size();
 
-        for(int i = 0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                return i;
+        if(n==1){
+            return 0;
+        }
+
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+
+        int low = 1, high = n - 2;
+
+        while(low<=high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                low = mid + 1;
+            }
+            else if(nums[mid]>nums[mid+1]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
 
-        return n - 1;
+        return -1;
         
     }
 };
