305bf5f9432d80e37bf5f2a2bca0eca72489d21e
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-26 13:00:19 +0530

    Time: 642 ms (65.68%) | Memory: 94.1 MB (52.98%) - LeetSync

--- 3699-count-special-subsequences/count-special-subsequences.cpp
+++ 3699-count-special-subsequences/count-special-subsequences.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     long long numberOfSubsequences(vector<int>& nums) {
 
-        unordered_map<double, long long> mp;
-
+        int n = nums.size();
         long long ans = 0;
 
-        int n = nums.size();
+        unordered_map<double,long long> mp;
 
         for(int r = 4;r<n-2;r++){
-
             int q = r - 2;
 
-            for(int p = 0;p<q - 1;p++){
-                double rat = (1.0 * nums[p])/(1.0 * nums[q]);
-
-                mp[rat]++;
+            for(int p = 0;p<q-1;p++){
+                double ratio = (nums[p]*1.0)/(nums[q]*1.0);
+                mp[ratio]++;
             }
 
-
-
-            for(int s = r + 2;s<n;s++){
-                double rat = (1.0 * nums[s])/(1.0 * nums[r]);
-
-                ans += mp[rat];
+            for(int s = r+2;s<n;s++){
+                double ratio = (nums[s]*1.0)/(nums[r]*1.0);
+                ans += mp[ratio];
             }
-
         }
 
         return ans;
-
-
         
     }
 };
