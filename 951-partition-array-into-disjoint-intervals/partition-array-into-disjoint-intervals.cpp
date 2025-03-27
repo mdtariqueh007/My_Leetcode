b51ae97e7b102700f2eec72cfb04390b4d6b96b7
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2025-03-28 03:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 102.2 MB (54.42%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -4,20 +4,21 @@ public:
 
         int n = nums.size();
 
-        vector<int> prefixMax(n), suffixMin(n);
+        vector<int> suffixMin(n);
 
-        prefixMax[0] = nums[0];
+        int prefixMax = nums[0];
         suffixMin[n-1] = nums[n-1];
 
         for(int i = 1;i<n;i++){
-            prefixMax[i] = max(nums[i], prefixMax[i-1]);
             suffixMin[n-i-1] = min(nums[n-i-1], suffixMin[n-i]);
         }
 
         for(int i = 0;i<n-1;i++){
-            if(prefixMax[i]<=suffixMin[i+1]){
+
+            if(prefixMax<=suffixMin[i+1]){
                 return i + 1;
             }
+            prefixMax = max(prefixMax, nums[i]);
         }
 
         return -1;
